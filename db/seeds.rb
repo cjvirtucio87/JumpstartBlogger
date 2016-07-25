4d034f4a1503fd5ef6f4b29f56c732c2fb43e0bc
cjvirtucio87 <cjv287@gmail.com>
2016-07-25 10:23:02 -0700

    Added seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+10.times do |n|
+  Article.create!(title: "Article ##{n}",
+                  body: "This is an article!")
+end
+
+10.times do |n|
+  Article.all.each do |article|
+    article.comments.push Comment.new(author: "A commenter number #{n}",
+                             body: "Comment number #{n}")
+  end
+end
